665d00d7e33cc4f3d19e9dafd5abf1ec8420ef53
Aaron Wolverton <aaron.wolverton@gmail.com>
2013-09-19 11:15:57 -0400

    Initial Commit

---
+++ BlogReader/BlogReaderAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BlogReaderAppDelegate.h
+//  BlogReader
+//
+//  Created by Aaron Wolverton on 9/19/13.
+//  Copyright (c) 2013 Aaron Wolverton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlogReaderAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader
+//
+//  Created by Aaron Wolverton on 9/19/13.
+//  Copyright (c) 2013 Aaron Wolverton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BlogReaderAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BlogReaderAppDelegate class]));
+    }
+}
